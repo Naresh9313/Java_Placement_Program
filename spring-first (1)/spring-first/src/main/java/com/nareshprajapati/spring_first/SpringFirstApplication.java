dca6af881c276d0401f75e2d2e2c2bc5f92faff9
Naresh Prajapati <112102056+Naresh9313@users.noreply.github.com>
2024-09-17 12:10:55 +0530

    Add files via upload

---
+++ spring-first (1)/spring-first/src/main/java/com/nareshprajapati/spring_first/SpringFirstApplication.java
@@ -0,0 +1,14 @@
+package com.nareshprajapati.spring_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstApplication.class, args);
+		console.log("naresh")
+	}
+
+}
